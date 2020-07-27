ae62b3139ca648a758583e572a85ec951c91c640
Anthony Baynham <anthonyybayn@gmail.com>
2020-07-27 14:52:53 +0100

    Switched back to gcc

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-compile="clang -std=c99 src/*.c -o lang -Wall -Wpedantic -Wno-switch -Wno-unused-function -g -ldl -lpthread -O0"
+compile="gcc -std=c99 src/*.c -o lang -Wall -Wpedantic -Wno-switch -Wno-unused-function -g -ldl -lpthread -O0"
 run="./lang scratch/1.lang"
 
 echo -e "\nCompiling:"
