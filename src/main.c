60b4f698474de28f1bd5981dfebfd419fb508d87
Anthony Baynham <anthonyybayn@gmail.com>
2020-08-24 19:59:04 +0100

    Cleaned up main

--- src/main.c
+++ src/main.c
@@ -13,10 +13,18 @@
 #include "headers/stb/stb_ds.h"
 #include "headers/stb/stretchy_buffer.h"
 
-#define perfstats 1
-
 static char *read_file(const char *path);
 static void print_unused_symbol_warnings(Context *);
+
+// Roughly time the execution of "code" in microseconds
+// There must be a variable called "id"_delta in the scope that you use this in
+#define PROFILE(id, code) do {\
+    struct timeval id ## end, id ## start;\
+    gettimeofday(&id##start, NULL);\
+    code\
+    gettimeofday(&id##end, NULL);\
+    id##_delta = (id##end.tv_sec - id##start.tv_sec) * 1000000 + id##end.tv_usec - id##start.tv_usec;\
+} while (false)
 
 // From parser.c, circular dependency in the header files.
 Ast parse(Context *);
@@ -24,7 +32,13 @@ Ast parse(Context *);
 // THINK: for imports, just lex, parse and resolve all the files and then merge their scopes into one scope, then generate code for the symbols, symbols that are Sym_UNRESOLVED, can be ignored.
 
 bool process_file(const char *file_path) {
+    #define NEXT_STAGE_OR_QUIT() if (context.error_count > 0) goto end;
+
     char *file_data = read_file(file_path); // read the file into a zero-terminated buffer.
+
+    u64 lexer_delta = 0;
+    u64 parser_delta = 0;
+    u64 checker_delta = 0;
 
     SourceStats stats = (SourceStats){10}; // initialize all the fields to 10
     TokenList tokens;
@@ -35,74 +49,34 @@ bool process_file(const char *file_path) {
     token_list_init(&tokens);
     lexer_init(&context.lexer, file_path, file_data);
 
-    u64 lex_delta = 0;
-    #if perfstats
-        struct timeval lend, lstart;
-        gettimeofday(&lstart, NULL);
-    #endif
-
-    if (!lexer_lex(&context, &tokens, &stats)) return 1;
-
-    #if perfstats
-        gettimeofday(&lend, NULL);
-        /* In microseconds */
-        lex_delta = (lend.tv_sec - lstart.tv_sec) * 1000000 + lend.tv_usec - lstart.tv_usec;
-    #endif
+    PROFILE(lexer, {
+        if (!lexer_lex(&context, &tokens, &stats)) return 1;
+    });
 
     if (tokens.len == 1) return 0;
 
     init_types(&context, &stats);
     parser_init(&context.parser, &tokens, &stats);
 
-    u64 parse_delta = 0;
-    #if perfstats
-        struct timeval pend, pstart;
-        gettimeofday(&pstart, NULL);
-    #endif
-
-    ast = parse(&context);
-
-    #if perfstats
-        gettimeofday(&pend, NULL);
-        /* In microseconds */
-        parse_delta = (pend.tv_sec - pstart.tv_sec) * 1000000 + pend.tv_usec - pstart.tv_usec;
-    #endif
+    PROFILE(parser, {
+        ast = parse(&context);
+    });
 
     token_list_free(&tokens);
 
-    if (context.error_count > 0) {
-        // dump_parser_errors(&context.parser);
-        //print_ast_errors(&context, &ast);
-        goto end;
-    }
-
-    u64 check_delta = 0;
-    #if perfstats
-        struct timeval cend, cstart;
-        gettimeofday(&cstart, NULL);
-    #endif
-
-    if (!context.decl_for_main) {
+    if (!context.decl_for_main)
         compile_error(&context, (Token){0}, "No entry point found. Please declare \"main\"");
-        goto end;
-    }
 
-    resolve_program(&context, context.decl_for_main);
+    NEXT_STAGE_OR_QUIT();
 
-    if (context.error_count > 0)
-        goto end;
+    PROFILE(checker, {
+        resolve_program(&context, context.decl_for_main);
+        NEXT_STAGE_OR_QUIT();
+        check_ast(&context, &ast); // type and semantic checking
+        NEXT_STAGE_OR_QUIT();
+    });
 
     print_unused_symbol_warnings(&context);
-    check_ast(&context, &ast); // type and semantic checking
-
-    if (context.error_count > 0)
-        goto end;
-
-    #if perfstats
-        gettimeofday(&cend, NULL);
-        /* In microseconds */
-        check_delta = (cend.tv_sec - cstart.tv_sec) * 1000000 + cend.tv_usec - cstart.tv_usec;
-    #endif
 
 end:
     {
@@ -113,10 +87,10 @@ end:
         }
 
         printf("Error count: %d\n", context.error_count);
-        printf("Total time: %ldus\n", lex_delta + parse_delta + check_delta);
-        printf("\tLexing took %ldus\n", lex_delta);
-        printf("\tParsing took %ldus\n", parse_delta);
-        printf("\tInferring, resolving and checking took %ldus\n", check_delta);
+        printf("Total time: %ldus\n", lexer_delta + parser_delta + checker_delta);
+        printf("\tLexing took %ldus\n", lexer_delta);
+        printf("\tParsing took %ldus\n", parser_delta);
+        printf("\tInferring, resolving and checking took %ldus\n", checker_delta);
         printf("Parser used %lu nodes out of %lu allocated.\n", context.parser.node_count, context.parser.node_allocator.capacity);
 
         free_types(&context);
@@ -152,6 +126,7 @@ static void print_unused_symbol_warnings(Context *ctx) {
             }
         }
     }
+    printf("\n");
 }
 
 static char *read_file(const char *path) {
